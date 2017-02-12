69e03f89f053d1d5c13b253bd2d401ccd15c3b4d
Jac Kersing <j.kersing@the-box.com>
2017-02-12 12:40:49 +0100

    Add missing files

---
+++ mp_pkt_fwd/inc/transport.h
@@ -0,0 +1,56 @@
+/*
+ * transport.h
+ * 
+ *  Created on: Feb 10, 2017
+ *      Author: Jac Kersing
+ */
+
+
+#ifndef _TRANSPORT_H
+#define _TRANSPORT_H
+enum server_type {
+    semtech,
+    ttn_gw_bridge
+} ;
+
+struct _queue {
+    struct _queue *next;
+    int nbpkt;
+    char *status;	    // pointer to semtech status report
+    struct lgw_pkt_rx_s data[NB_PKT_MAX];
+};
+
+typedef struct _queue Queue;
+
+struct _server {
+    enum server_type type;  // type of server
+    bool enabled;           // server enabled
+    bool upstream;          // upstream enabled
+    bool downstream;        // downstream enabled
+    bool statusstream;      // status stream enabled
+    char addr[64];          // server address
+    char port_up[8];        // uplink port for semtech proto
+    char port_down[8];      // downlink port for semtech proto
+    int  max_stall;         // max number of missed responses
+    char gw_id[64];         // gateway ID for TTN
+    char gw_key[200];       // gateway key to connect to TTN
+    bool live;              // Server is life?
+    int  sock_up;           // Semtech up socket
+    int  sock_down;         // Semtech down socket
+    sem_t send_sem;         // semaphore for sending data
+    pthread_t t_down;	    // semtech down thread
+    pthread_t t_up;	    // upstream thread
+    time_t contact;         // time of last contact
+    Queue *queue;           // queue of packets uplink data
+    TTN *ttn;               // TTN connection object
+};
+typedef struct _server Server;
+
+void transport_init();
+void transport_start();
+void transport_stop();
+void transport_data_up(int nb_pkt, struct lgw_pkt_rx_s *rxpkt, bool send_report);
+void transport_status_up(uint32_t, uint32_t, uint32_t, uint32_t);
+#endif // _TRANSPORT_H
+
+
---
+++ mp_pkt_fwd/src/transport.c
@@ -0,0 +1,118 @@
+/*
+ * transport.c
+ *
+ *  Created on: Feb 10, 2017
+ *      Author: Jac Kersing
+ */
+
+#include <stdint.h>         /* C99 types */
+#include <stdbool.h>        /* bool type */
+#include <stdio.h>          /* printf, fprintf, snprintf, fopen, fputs */
+
+#include <string.h>         /* memset */
+#include <signal.h>         /* sigaction */
+#include <time.h>           /* time, clock_gettime, strftime, gmtime */
+#include <sys/time.h>       /* timeval */
+#include <unistd.h>         /* getopt, access */
+#include <stdlib.h>         /* atoi, exit */
+#include <errno.h>          /* error messages */
+#include <math.h>           /* modf */
+#include <assert.h>
+
+#include <pthread.h>
+#include <semaphore.h>
+
+#include "mp_pkt_fwd.h"
+#include "trace.h"
+#include "loragw_hal.h"
+#include "connector.h"
+#include "transport.h"
+#include "semtech_transport.h"
+#include "ttn_transport.h"
+
+extern int serv_count;
+extern Server servers[];
+
+void transport_init();
+void transport_start();
+void transport_stop();
+void transport_data_up(int nb_pkt, struct lgw_pkt_rx_s *rxpkt, bool send_report);
+
+// Initialize all data structures
+void transport_init() {
+	int i;
+	for (i = 0; i < MAX_SERVERS; i++) {
+		memset(&servers[i], 1, sizeof(Server));
+		servers[i].type = semtech;
+		servers[i].enabled = false;
+		servers[i].upstream = true;
+		servers[i].downstream = true;
+		servers[i].statusstream = true;
+		servers[i].live = false;
+		servers[i].sock_up = -1;
+		servers[i].sock_down = -1;
+		servers[i].queue = NULL;
+		sem_init(&servers[i].send_sem, 0, 0);
+	}
+}
+
+void transport_start() {
+	int i;
+	MSG("INFO: [Transports] Initializing protocol for %d servers\n",serv_count);
+	for (i = 0; i < MAX_SERVERS; i++) {
+		if (servers[i].enabled == true) {
+			switch (servers[i].type) {
+				case semtech: 
+					semtech_init(i);
+					break;
+				case ttn_gw_bridge:
+					ttn_init(i);
+					break;
+			}
+		}
+	}
+}
+
+void transport_stop() {
+	int i;
+	for (i = 0; i < MAX_SERVERS; i++) {
+		if (servers[i].enabled == true) {
+			switch (servers[i].type) {
+				case semtech:
+					semtech_stop(i);
+					break;
+				case ttn_gw_bridge:
+					ttn_stop(i);
+					break;
+			}
+		}
+	}
+}
+
+void transport_data_up(int nb_pkt, struct lgw_pkt_rx_s *rxpkt, bool send_report) {
+	int i;
+	for (i = 0; i < MAX_SERVERS; i++) {
+		if (servers[i].enabled == true && servers[i].upstream == true) {
+			switch (servers[i].type) {
+				case semtech:
+					semtech_data_up(i, nb_pkt, rxpkt, send_report);
+					break;
+				case ttn_gw_bridge:
+					ttn_data_up(i, nb_pkt, rxpkt);
+					break;
+			}
+		}
+	}
+}
+
+void transport_status_up(uint32_t rx_rcv,uint32_t rx_ok,uint32_t tx_tot,uint32_t tx_ok) {
+	int i;
+	for (i = 0; i < MAX_SERVERS; i++) {
+		if (servers[i].enabled == true && servers[i].type == ttn_gw_bridge && servers[i].statusstream == true) {
+			ttn_status_up(i, rx_rcv, rx_ok, tx_tot, tx_ok);
+		}
+	}
+}
+
+// vi: ts=4 sw=4
+
